81d78173101c7a69a28aec7310f917419f58c0f4
saiaditya00 <kamarsuaditya@gmail.com>
2024-09-16 10:39:06 +0530

    Initial commit

---
+++ src/main/kotlin/model/Task.kt
@@ -0,0 +1,16 @@
+package model
+
+
+
+import kotlinx.serialization.Serializable
+
+enum class Priority {
+    Low, Medium, High, Vital
+}
+
+@Serializable
+data class Task(
+    val name: String,
+    val description: String,
+    val priority: Priority
+)
